83f1b130db8f706176e95bc47b92295035553831
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-06-26 16:45:58 -0700

    Create Custom_Field model

--- server/models/custom_field.py
+++ server/models/custom_field.py
@@ -2,3 +2,12 @@ from django.db import models
 from .adventure import Adventure
 from .scene import Scene
 from .encounter import Encounter
+
+class Custom_Field(models.Model):
+    safe_text_validator = Adventure.safe_text_validator
+    
+    adventure_id = models.ForeignKey(Adventure, on_delete=models.CASCADE)
+    scene_id = models.ForeignKey(Scene, on_delete=models.CASCADE)
+    encounter_id = models.ForeignKey(Encounter, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 80, validators = [safe_text_validator])
+    value = models.TextField(blank = True, validators = [safe_text_validator])
