78ffe6768e6d46357dfee6a3199bba627fd3c60e
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-06-26 21:53:03 -0700

    register models with admin site

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Adventure
+from .models import Scene
+from .models import Encounter
+from .models import Custom_Field
 
 # Register your models here.
+admin.site.register(Adventure)
+admin.site.register(Scene)
+admin.site.register(Encounter)
+admin.site.register(Custom_Field)
