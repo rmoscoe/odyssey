abc867df0deabaaf7b45bea9c42871b618efa270
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-06-27 14:45:01 -0700

    create viewsets

--- server/views.py
+++ server/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from server.models import Adventure, Scene, Encounter, Custom_Field
+from django.contrib.auth.models import User
+from server.serializers import AdventureSerializer, UserSerializer, SceneSerializer, EncounterSerializer, CustomFieldSerializer
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, "index.html", context)
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class AdventureViewSet(viewsets.ModelViewSet):
+
+    queryset = Adventure.objects.all()
+    serializer_class = AdventureSerializer
+
+class SceneViewSet(viewsets.ModelViewSet):
+
+    queryset = Scene.objects.all()
+    serializer_class = SceneSerializer
+
+class EncounterViewSet(viewsets.ModelViewSet):
+
+    queryset = Encounter.objects.all()
+    serializer_class = EncounterSerializer
+
+class CustomFieldViewSet(viewsets.ModelViewSet):
+
+    queryset = Custom_Field.objects.all()
+    serializer_class = CustomFieldSerializer
