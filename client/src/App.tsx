40f921b8dc54e23fb8daa5674f63c1e2564247ae
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-06-29 16:25:48 -0700

    build out App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PageSelector from './components/PageSelector';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const handlePageChange = (page: string) => setCurrentPage(page);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main>
+        <PageSelector currentPage={currentPage} />
+      </main>
+      <Footer />
+    </>
   );
 }
 
