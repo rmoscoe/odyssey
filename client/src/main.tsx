0b0ba0d7a1b93684c88ac3a991367ea5bd4e8d8c
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-07-11 13:59:59 -0700

    develop main.tsx and App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import ThemeProvider from './utils/ThemeContext';
 
-const rootElement: Element | DocumentFragment | null = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement as Element | DocumentFragment).render(<App />);
-}
+// const rootElement: Element | DocumentFragment | null = document.getElementById('root');
+// if (rootElement) {
+//   createRoot(rootElement as Element | DocumentFragment).render(<App />);
+// }
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
