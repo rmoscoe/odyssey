13f24e308e754a666f469177fb1f0442926f71d6
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-07-13 09:43:30 -0700

    refine styling for Login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -25,12 +25,12 @@ export default function Login({ handlePageChange }: PageProps) {
         e.preventDefault();
 
         const { target } = e;
-        const inputType = target.name;
+        const inputId = target.id;
         const inputValue = target.value;
 
         target.classList.remove("invalid-entry");
 
-        if (inputType === 'email') {
+        if (inputId === 'email-field') {
             setEmail(inputValue);
         } else {
             setPassword(inputValue);
@@ -38,12 +38,12 @@ export default function Login({ handlePageChange }: PageProps) {
 
         switch (notification) {
             case 'Please enter a valid email address':
-                if (inputType === 'email') {
+                if (inputId === 'email-field') {
                     setNotification('');
                 }
                 break;
             case 'Please enter your password or click "Forgot Password?"':
-                if (inputType === 'password') {
+                if (inputId === 'password-field') {
                     setNotification('');
                 }
                 break;
@@ -100,63 +100,67 @@ export default function Login({ handlePageChange }: PageProps) {
             error === 'Invalid email or password' ? setNotification('Invalid email or password') : setNotification('An error occured while logging in. Please try again.');
             console.error(error);
         }
-        
+
         setEmail('');
         setPassword('');
     }
 
     return (
-        <main className="mt-[6.5rem] w-full flex content-center p-1.5 h-screen">
-            <section className={`bg-${theme}-contrast rounded-3xl h-[90%] w-[97%] lg:w-3/5`}>
-                <h2 className={`font-${theme}-heading text-${theme}-form-heading text-3xl mx-auto mb-5 lg:text-4xl`}>Log in to Odyssey</h2>
+        <main className="mt-[5.5rem] w-full flex justify-center p-2 h-overlay relative">
+            <section className={`absolute top-[2%] bottom-7 inset-x-2.5 bg-${theme}-contrast rounded-[2rem] justify-center p-3 lg:w-3/5 lg:mx-auto`}>
+                <h2 className={`font-${theme}-heading text-center text-${theme}-form-heading text-[1.75rem] mx-auto mb-5 lg:text-4xl`}>Log in to Odyssey</h2>
                 <p className={`text-center mx-auto w-[95%] mb-4 ${theme}-text lg:w-3/5`}>The journey continues. Speak friend and enter.</p>
                 {notification === 'An error occured while logging in. Please try again.' &&
                     <p className={`text-center mx-auto w-[95%] mb-4 ${theme}-text lg:w-3/5`}>{notification}</p>
                 }
                 <form autoComplete="on" id="login-form" className="mx-auto w-[95%] lg:w-3/5" onSubmit={handleLoginSubmit}>
-                    <label htmlFor="email-field" className={`${theme}-label mb-2`}>Email</label>
-                    <input
-                        type="email"
-                        id="email-field"
-                        name="email-field"
-                        pattern="^([a-z0-9]{1})([a-z0-9_.!#$%&'*+-/=?^`{|}~]{0,63})@([\da-z.-]{1,253})\.([a-z.]{2,6})$"
-                        className={`bg-${theme}-field border-${theme}-primary border-[3px] rounded-lg text-${theme}-text w-full`}
-                        value={email}
-                        onChange={handleInputChange}
-                        onBlur={handleEmailLoseFocus}
-                        required
-                    />
-                    {notification === 'Please enter a valid email address' &&
-                        <p className={`${theme}-text`}>{notification}</p>
-                    }
-                    <label htmlFor="password-field" className={`${theme}-label mt-4 mb-2`}>Password</label>
-                    <input
-                        type="password"
-                        autoComplete="current-password"
-                        id="password-field"
-                        name="password-field"
-                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)$"
-                        minLength={8}
-                        className={`bg-${theme}-field border-${theme}-primary border-[3px] rounded-lg text-${theme}-text w-full`}
-                        value={password}
-                        onChange={handleInputChange}
-                        onBlur={handlePasswordLoseFocus}
-                        required
-                    />
-                    <Link to='/account/reset-password' className={`ml-auto mr-1 font${theme}-text`}>Forgot Password?</Link>
-                    {notification === 'Please enter your password or click "Forgot Password?"' &&
-                        <p className={`${theme}-text`}>{notification}</p>
-                    }
+                    <div className="mb-4">
+                        <label htmlFor="email-field" className={`${theme}-label`}>Email</label>
+                        <input
+                            type="email"
+                            id="email-field"
+                            name="email-field"
+                            pattern="^([a-z0-9]{1})([a-z0-9_.!#$%&'*+-/=?^`{|}~]{0,63})@([\da-z.-]{1,253})\.([a-z.]{2,6})$"
+                            className={`bg-${theme}-field border-${theme}-primary border-[3px] rounded-xl text-${theme}-text w-full text-lg px-1 py-2 mt-2`}
+                            value={email}
+                            onChange={handleInputChange}
+                            onBlur={handleEmailLoseFocus}
+                            required
+                        />
+                        {notification === 'Please enter a valid email address' &&
+                            <p className={`${theme}-text mt-2`}>{notification}</p>
+                        }
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password-field" className={`${theme}-label`}>Password</label>
+                        <input
+                            type="password"
+                            autoComplete="current-password"
+                            id="password-field"
+                            name="password-field"
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)$"
+                            minLength={8}
+                            className={`bg-${theme}-field border-${theme}-primary border-[3px] rounded-xl text-${theme}-text w-full text-lg px-1 py-2 mt-2`}
+                            value={password}
+                            onChange={handleInputChange}
+                            onBlur={handlePasswordLoseFocus}
+                            required
+                        />
+                        <Link to='/account/reset-password' className={`block mt-1 ml-auto w-full px-1 text-right font-${theme}-text link-text`}>Forgot Password?</Link>
+                        {notification === 'Please enter your password or click "Forgot Password?"' &&
+                            <p className={`${theme}-text mt-2`}>{notification}</p>
+                        }
+                    </div>
                     <input
                         type="submit"
                         id="submit-login"
-                        className={`mt-4 w-full border-${theme}-button-alt-border border-[3px] rounded-2xl bg-${theme}-primary text-${theme}-accent font-${theme}-text`}
+                        className={`mt-4 py-2 w-full border-${theme}-button-alt-border border-[3px] rounded-xl bg-${theme}-primary text-${theme}-accent text-lg font-${theme}-text`}
                         value="Submit"
                     />
                 </form>
                 <p className={`${theme}-text mt-4 mx-auto w-[95%] lg:w-3/5`}>
-                    New to Odyssey?
-                    <Link to="/account/new">Create an account for FREE.</Link>
+                    New to Odyssey? &nbsp;
+                    <Link className="link-text" to="/account/new">Create an account for FREE.</Link>
                 </p>
             </section>
         </main>
