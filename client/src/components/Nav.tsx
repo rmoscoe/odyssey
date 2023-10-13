9c23d0ccfe0392aa5908a3a46439efead0ecb112
Ryan Moscoe <ryanmoscoe@gmail.com>
2023-10-12 23:21:28 -0700

    hide toggle input

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -51,15 +51,16 @@ const Nav = ({ currentPage, handleLogout }: NavProps) => {
                     <Sword color={theme==='fantasy' ? '#F7CE65' : '#FF54A4'}/>
                 </svg>
                 <label className={`relative inline-block w-16 h-7 p-2`}>
+                    <div className={`absolute appearance-none mx-2 cursor-pointer top-0 right-0 bottom-0 left-0 bg-${theme}-toggle-void border-${theme}-toggle-border border-4 rounded-[34px] duration-300 peer focus:outline-none`}></div>
                     <input 
                         type="checkbox" 
                         role="switch"
                         id="theme-toggle" 
                         checked={theme === 'sci-fi'} 
-                        className={`absolute appearance-none mx-2 cursor-pointer top-0 right-0 bottom-0 left-0 bg-${theme}-toggle-void border-${theme}-toggle-border border-4 rounded-[34px] duration-300 peer focus:outline-none`} 
+                        className={`peer/toggle hidden`} 
                         onChange={toggleTheme} 
                     />
-                    <span className={`absolute left-3 top-1 h-5 w-5 rounded-full transition-transform duration-300 transform peer-checked:translate-x-5 bg-${theme}-toggle-switch cursor-pointer`}></span>
+                    <span className={`absolute left-3 top-1 h-5 w-5 rounded-full transition-transform duration-300 transform peer-checked/toggle:translate-x-5 bg-${theme}-toggle-switch cursor-pointer`}></span>
                 </label>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 100 100">
                     <Planet color={theme==='fantasy' ? '#F7CE65' : '#FF54A4'}/>
