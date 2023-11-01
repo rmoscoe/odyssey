8d79e02f26f7d0366f68b3b03def52b790a30bc0
Ryan Moscoe <ryanmoscoe@gmail.com>
2023-11-01 11:57:46 -0700

    trying to debug Adventure Details not saving updates

--- client/src/components/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { useTheme } from '../utils/ThemeContext';
-
-export default function Spinner() {
-    const { theme } = useTheme();
-
-    return (
-        <div className={`${theme}-spinner z-30`}></div>
-    );
-}
